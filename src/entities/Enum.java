bf93dc6eec56d9ec9169ee01444b3fc4d6d4ac18
Youssef Awad <23P0245@eng.asu.edu.eg>
2025-12-03 21:35:00 +0200

    Add Enum

---
+++ src/entities/Enum.java
@@ -0,0 +1,8 @@
+package src.entities;
+
+public class Enum {
+    enum Status { TODO, IN_PROGRESS, DONE, VERIFIED, REOPENED } 
+    enum UserRole { STAKEHOLDER, DEVELOPER, QA, SCRUM_MASTER } 
+    enum WorkItemType { EPIC, STORY, TASK, BUG } 
+    enum Severity { LOW, MEDIUM, HIGH, CRITICAL } 
+}
